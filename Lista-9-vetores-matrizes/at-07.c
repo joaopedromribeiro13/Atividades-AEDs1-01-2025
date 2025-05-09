283b0c78108c6444089c5fcf7dcd4bff1d5a0779
João Pedro <107947773+joaopedromribeiro13@users.noreply.github.com>
2025-05-09 19:48:47 -0300

    commit final lista 9

--- Lista-9-vetores-matrizes/at-07.c
+++ Lista-9-vetores-matrizes/at-07.c
@@ -3,35 +3,52 @@
 #include <stdlib.h>
 #define max 100
 
-//B
+// B
 void preencheMatriz(int matriz[][max], int n) {
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<n; j++) {
-            matriz[i][j]=rand()%100+1;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            matriz[i][j] = rand() % 100 + 1;
         }
     }
 }
 
-//C
+// C
 void somaDiagonalPrincipal(int matriz[][max], int n) {
-    int soma=0;
-    for(int i=0; i<n; i++) {
-        soma+=matriz[i][i];
+    int soma = 0;
+    for (int i = 0; i < n; i++) {
+        soma += matriz[i][i];
     }
-    printf("Soma diagonal principal: %d\n", soma);
+    printf("Soma da diagonal principal: %d\n", soma);
 }
 
+// D
+void somaAbaixoDiagonal(int *matriz, int n) {
+    int soma = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            soma += *(matriz + i * n + j);
+        }
+    }
+    printf("Soma abaixo da diagonal principal: %d\n", soma);
+}
 
 int main() {
-    //A
+    // A
     int n;
-    printf("Digite um numero: ");
+    printf("Digite o tamanho da matriz quadrada: ");
     scanf("%d", &n);
+
+    if (n <= 0 || n > max) {
+        printf("Tamanho invalido.\n");
+        return 1;
+    }
 
     int matriz[max][max];
     srand(time(NULL));
+
     preencheMatriz(matriz, n);
     somaDiagonalPrincipal(matriz, n);
+    somaAbaixoDiagonal(&matriz[0][0], n);
 
-return 0;
+    return 0;
 }
